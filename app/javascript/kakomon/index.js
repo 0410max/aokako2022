115778a8c4fa0a0413abed278830da1cd5192e7f
Hashimoto Taiga <2002421taiga@gmail.com>
2022-03-02 09:58:57 +0900

    3.2.10

--- app/javascript/kakomon/index.js
+++ app/javascript/kakomon/index.js
@@ -1,17 +1,29 @@
 class IndexModal{
-    constructor(rootElm){
-        this.rootElm = rootElm
-    }
 
     init(){
-        const indexModal = this.rootElm.querySelector('#index-modal');
+        const indexModal = document.querySelector('.index-modal-button');
         indexModal.addEventListener('click',()=>{
-            alert('test');
+            const indexModalElm = document.querySelector('.index-modal');
+            indexModalElm.innerHTML = this.indexModalContent();
         });
+    }
+
+    indexModalContent(){
+        return`
+            <div class='modal-wrapper'>
+                <div class='index-modal-bg'>
+                    <div class='modal-content'>
+                        <div>報告する</div><br />
+                        <div>投稿へ移動</div><br />
+                        <div>キャンセル</div>
+                    </div>
+                </div>
+            </div>
+        `;
     }
 }
 
 document.addEventListener('turbolinks:load',()=>{
-    const indexModal = new IndexModal(document.querySelector('#modalContent'));
+    const indexModal = new IndexModal();
     indexModal.init();
 });
