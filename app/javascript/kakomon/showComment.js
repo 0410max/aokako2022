b9019e6f14cc520a8a7165c736159dea6be13717
Hashimoto Taiga <2002421taiga@gmail.com>
2022-05-21 19:55:49 +0900

    5.21.19

--- app/javascript/kakomon/showComment.js
+++ app/javascript/kakomon/showComment.js
@@ -1,21 +1,26 @@
 document.addEventListener("turbolinks:load", function () {
-$(function(){
+    $(function(){
 
-    $('.kakomons-show-top-button')
-        .on('click',function(){
-            $('.index-modal,.index-modal-bg').fadeIn();
-            $("body").addClass("no_scroll"); 
-        });
+        $('.kakomons-show-top-button')
+            .on('click',function(){
+                $('.index-modal,.index-modal-bg').fadeIn();
+                $("body").addClass("no_scroll"); 
+            });
 
-        $('.index-modal-bg,.index-modal-close-button')
-        .on('click',function(){
-            $("body").removeClass("no_scroll");
-            $('.index-modal,.index-modal-bg').fadeOut();
-        });
+            $('.index-modal-bg,.index-modal-close-button')
+            .on('click',function(){
+                $("body").removeClass("no_scroll");
+                $('.index-modal,.index-modal-bg').fadeOut();
+            });
 
-    $('.kakomons-show-text-area')
-        .on('input',function(){
-            $('.kakomons-show-comment-submit,.kakomons-show-comment-submit-1000').css('opacity',1)
-        });
-});    
+        $('.kakomons-show-text-area')
+            .on('input',function(){
+                let text = $(this).val();
+                if(text != ''){
+                    $('.kakomons-show-comment-submit,.kakomons-show-comment-submit-1000').css('opacity',1);
+                }else{
+                    $('.kakomons-show-comment-submit,.kakomons-show-comment-submit-1000').css('opacity',0.5);
+                }
+            });
+    });    
 });
